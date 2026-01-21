9e9971add8fb23e2160c524ed08e7c53e505de8f
Parshva <codewithpasshu@gmail.com>
2026-01-21 18:39:42 +0530

    Projec Completed

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ function dailyPlanner() {
   let dayPlanData = JSON.parse(localStorage.getItem("dayPlanData")) || {};
   var hours = Array.from(
     { length: 18 },
-    (elem, idx) => `${6 + idx}:00 - ${7 + idx}:00`
+    (elem, idx) => `${6 + idx}:00 - ${7 + idx}:00`,
   );
 
   let wholeDaySum = "";
@@ -134,7 +134,7 @@ function pomoDoroTimer() {
     let minutes = Math.floor(totalSeconds / 60);
     let seconds = totalSeconds % 60;
     timer.innerHTML = `${String(minutes).padStart("2", "0")}:${String(
-      seconds
+      seconds,
     ).padStart("2", "0")}`;
   }
 
@@ -184,3 +184,123 @@ function pomoDoroTimer() {
   resetBtn.addEventListener("click", resetTimer);
 }
 pomoDoroTimer();
+
+function weatherFunctionality() {
+  var time = document.querySelector(".header1 h1");
+  var dateh2 = document.querySelector(".header1 h2");
+  var headerh2Temp = document.querySelector(".header2 h2");
+  var headerh4Temp = document.querySelector(".header2 h4");
+  var cityh4 = document.querySelector(".header1 h4");
+  var presi = document.querySelector(".header2 .preci");
+  var humidity = document.querySelector(".header2 .humi");
+  var wind = document.querySelector(".header2 .wind");
+  var data = null;
+  async function weatherAPICall() {
+    var key = "d17f0cebf61d4f47a7353806261701";
+    var city = "Visnagar";
+    var response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`,
+    );
+    data = await response.json();
+    console.log(data);
+
+    headerh2Temp.innerHTML = `${data.current.temp_c}°C`;
+    headerh4Temp.innerHTML = `${data.current.condition.text}`;
+
+    cityh4.innerHTML = `${city}`;
+    wind.innerHTML = `Wind: ${data.current.wind_kph} km/h`;
+    humidity.innerHTML = `Humidity: ${data.current.humidity}%`;
+    presi.innerHTML = `Precipitation: ${data.current.heatindex_c}%`;
+  }
+
+  weatherAPICall();
+
+  function timeDate() {
+    const totalDaysOfweek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    var date = new Date();
+    var dayofweek = totalDaysOfweek[date.getDay()];
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var odate = date.getDate();
+    var month = months[date.getMonth()];
+    var year = date.getFullYear();
+
+    dateh2.innerHTML = `${odate} ${month}, ${year}`;
+
+    if (hours > 12) {
+      time.innerHTML = `${dayofweek}, ${String(hours - 12).padStart("2", "0")}:${String(minutes).padStart("2", "0")}:${String(seconds).padStart("2", "0")} PM `;
+    } else {
+      time.innerHTML = `${dayofweek}, ${String(hours).padStart("2", "0")}:${String(minutes).padStart("2", "0")}:${String(seconds).padStart("2", "0")} AM `;
+    }
+  }
+
+  setInterval(() => {
+    timeDate();
+  }, 1000);
+}
+weatherFunctionality();
+
+function changeTheme() {
+  var theme = document.querySelector(".theme");
+  var rootElement = document.documentElement;
+  var flag = 0;
+
+  theme.addEventListener("click", function () {
+    // --pri: #f8f4e1;
+    // --sec: #381c0a;
+    // --tri1: #feba17;
+    // --tri2: #72512d;
+
+    if (flag == 0) {
+      rootElement.style.setProperty("--pri", "#F8F4E1");
+      rootElement.style.setProperty("--sec", "#222831");
+      rootElement.style.setProperty("--tri1", "#948979");
+      rootElement.style.setProperty("--tri2", "#393E46");
+
+      flag = 1;
+    } else if (flag == 1) {
+      rootElement.style.setProperty("--pri", "#F8F4E1");
+      rootElement.style.setProperty("--sec", "#40534C");
+      rootElement.style.setProperty("--tri1", "#677D6A");
+      rootElement.style.setProperty("--tri2", "#D6BD98");
+      flag = 2;
+    } else if (flag == 2) {
+      rootElement.style.setProperty("--pri", "#ECFAE5");
+      rootElement.style.setProperty("--sec", "#DDF6D2");
+      rootElement.style.setProperty("--tri1", "#CAE8BD");
+      rootElement.style.setProperty("--tri2", "#B0DB9C");
+      flag = 3;
+    } else if (flag == 3) {
+      rootElement.style.setProperty("--pri", "#f8f4e1");
+      rootElement.style.setProperty("--sec", "#381c0a");
+      rootElement.style.setProperty("--tri1", "#feba17");
+      rootElement.style.setProperty("--tri2", "#72512d");
+      flag = 0;
+    }
+  });
+}
+
+changeTheme();
